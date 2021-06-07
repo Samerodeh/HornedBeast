a31856c5163e79e9a717f96633b6cccba9363fa4
Samer Odeh <samerodeh220@gmail.com>
2021-06-08 01:51:49 +0300

    solve lab 02

--- my-app/src/components/HornedBeast.js
+++ my-app/src/components/HornedBeast.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            NumOfClicks: 0
+        }
+    }
+
+    NumOfClicks = () => {
+        let value = this.state.NumOfClicks;
+        this.setState({ NumOfClicks: value += 1 })
+    }
+
     render() {
         return (
-
             <div>
                 <h2>{this.props.title}</h2>
-                <img src={this.props.img} alt={this.props.keyword} onClick={this.vote} />
-                <p>{this.props.description}</p>
+                <img src={this.props.img} alt={this.props.keyword} onClick={this.NumOfClicks} />
+                <p> {this.props.description}</p>
+                <p> {this.state.NumOfClicks} </p>
             </div>
-
         )
     }
 }
+
+
 
 export default HornedBeast;
 
